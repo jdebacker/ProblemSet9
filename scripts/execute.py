1f4796369555354299ba09701f3b9af76660e213
jtregde <jmtregde@icloud.com>
2019-12-09 11:09:56 -0500

    Added parameter definitions from the households file.

--- scripts/execute.py
+++ scripts/execute.py
@@ -1,8 +1,10 @@
 # import packages
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import SS
 import TPI
+import demographics as demog
+import elliptical_u_est
 
 # model parameters
 S = int(40)
@@ -12,11 +14,14 @@ annual_beta = 0.9
 beta = annual_beta ** (40 / S)
 sigma = 3.0
 chi_n = 1.0 * np.ones(S)
-l_tilde = 1.0   # per period endowment for agent
+l_tilde = 1.0   # per period endowment for agent/ maximum labor supply
 b_s = 0.6
 upsilon = 1.5
+theta = 0.9  # frisch elasticity of labor
+# get estimated b and upsilon from ellipitical utility function
+b_ellip, upsilon = elliptical_u_est.estimation(theta, l_tilde)
 
-# Firm 
+# Firm
 A = 1.0
 annual_delta = 0.05
 delta = 1 - ((1 - annual_delta) ** (40 / S))
@@ -28,7 +33,8 @@ max_age = 100
 start_year = 2013
 pop_graphs = False
 (omega_path_S, imm_rates_path, rho_s, omega_SS, surv_rates_S, g_n_path,
-    g_n_SS, omega_S_preTP) = demog.get_pop_objs(E, S, T, min_age, max_age, start_year, pop_graphs)
+    g_n_SS, omega_S_preTP) = demog.get_pop_objs(E, S, T, min_age, max_age,
+                                                start_year, pop_graphs)
 imm_rates_SS = imm_rates_path[-1, :]
 
 # Economic growth
