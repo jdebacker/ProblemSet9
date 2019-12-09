75722bd421a6011009480ede452394a1d858d9c0
saharnazb <saharnaz.babaei@grad.moore.sc.edu>
2019-12-08 20:09:39 -0500

    update firm and add SS.py and LATEX

--- scripts/firm.py
+++ scripts/firm.py
@@ -1,25 +1,18 @@
 # firm functions
 
-def get_r(L, K, G, alpha, A, delta):
+def get_r(L, K, alpha, A, delta):
     '''
     The interest rate implied by the firm FOC for the choice of capital
-    G is the efficiency of labor with constant growth rate of g_y
-    I am going to define G in TPI as:
-          G = exp(g_y * t)
-          Or
-          G[t+1] = G[t] + G[t] * g_y with a for loop for t in range of S
     '''
-    r = alpha * A * G**(1-alpha) * (L/K)**(1-alpha) - delta
+    r = alpha * A * (L / K) ** (1 - alpha) - delta
 
     return r
 
 
-def get_w(L, K, G, alpha, A):
+def get_w(r, alpha, A, delta):
     '''
     The wage rate implied by the firm FOC for the choice of labor
-    G is the efficiency of labor with constant growth rate of g_y
-    NO NEED for delta here. Consider that in using this function.
     '''
-    w = (1 - alpha) * A *  G**(1-alpha) * (K/L)**(alpha) 
+    w = ((1 - alpha) * A * (K/L)**alpha
 
     return w
