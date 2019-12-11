1b7e7312bbb787a121b77f5a8330c26671cea10e
saharnazb <saharnaz.babaei@grad.moore.sc.edu>
2019-12-11 13:23:03 -0500

    TPI in progress

--- scripts/TPI.py
+++ scripts/TPI.py
@@ -9,7 +9,7 @@ def solve_tp(r_path_init, w_path_init, params):
     '''
     Solves for the time path equlibrium using TPI
     '''
-    (beta, sigma, alpha, A, delta, T, xi, b_sp1_pre, r_ss, w_ss,
+    (beta, sigma, alpha, A, delta, theta, T, xi, b_sp1_pre, r_ss, w_ss,
      b_sp1_ss) = params
     tpi_dist = 7.0
     tpi_tol = 1e-8
@@ -22,44 +22,4 @@ def solve_tp(r_path_init, w_path_init, params):
         # Solve HH problem
         b_sp1_mat = np.zeros((T + S - 1, S))
         euler_errors_mat = np.zeros((T + S - 1, S))
-        # solve upper right corner
-        foc_args = (beta, sigma, r_path[:2], w_path[:2], n[-2:],
-                    b_sp1_pre[0])
-        b_sp1_guess = b_sp1_ss[-1]
-        result = opt.root(hh.FOCs, b_sp1_guess, args=foc_args)
-        b_sp1_mat[0, -1] = result.x
-        euler_errors_mat[0, -1] = result.fun
-        # solve all full lifetimes
-        DiagMaskb = np.eye(2, dtype=bool)
-        for t in range(T):
-            foc_args = (beta, sigma, r_path[t:t+3], w_path[t:t+3], n,
-                        0.0)
-            b_sp1_guess = b_sp1_ss
-            result = opt.root(hh.FOCs, b_sp1_guess, args=foc_args)
-            b_sp1_mat[t:t+2, :] = (DiagMaskb * result.x +
-                                   b_sp1_mat[t:t+2, :])
-            euler_errors_mat[t:t+2, :] = (
-                DiagMaskb * result.fun + euler_errors_mat[t:t+2, :])
-        # create a b_s_mat
-        b_s_mat = np.zeros((T, 3))
-        b_s_mat[0, 1:] = b_sp1_pre
-        b_s_mat[1:, 1:] = b_sp1_mat[:T-1, :]
-        # use market clearing
-        L_path = np.ones(T) * agg.get_L(n)
-        K_path = agg.get_K(b_s_mat)
-        # find implied r
-        r_path_prime = firm.get_r(L_path, K_path, alpha, A, delta)
-        # check distance
-        tpi_dist = np.absolute(r_path[:T] - r_path_prime[:T]).max()
-        print('Iteration = ', tpi_iter, ', Distance = ', tpi_dist)
-        # update r
-        r_path[:T] = xi * r_path_prime[:T] + (1 - xi) * r_path[:T]
-        # update iteration counter
-        tpi_iter += 1
-
-    if tpi_iter < tpi_max_iter:
-        print('The time path solved')
-    else:
-        print('The time path did not solve')
-
-    return r_path[:T], euler_errors_mat[:T, :]
+        
