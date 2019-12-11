207caab128c671c00682a74289f90e9603928c81
saharnazb <saharnaz.babaei@grad.moore.sc.edu>
2019-12-11 13:28:18 -0500

    reference to equations for firm

--- scripts/firm.py
+++ scripts/firm.py
@@ -3,6 +3,7 @@
 def get_r(L, K, alpha, A, delta):
     '''
     The interest rate implied by the firm FOC for the choice of capital
+    Referring to equation (4.13)
     '''
     r = alpha * A * (L / K) ** (1 - alpha) - delta
 
@@ -12,6 +13,7 @@ def get_r(L, K, alpha, A, delta):
 def get_w(L, K, alpha, A, delta):
     '''
     The wage rate implied by the firm FOC for the choice of labor
+    Referring to Equation (4.14)
     '''
     w = (1 - alpha) * A * (K/L)**alpha
 
