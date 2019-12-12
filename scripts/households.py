fc29855dfbaa0b11f807b1e40754a651611c3f0d
jtregde <jmtregde@icloud.com>
2019-12-12 10:42:50 -0500

    corrected the FOCs function and renamed 'nu' to 'upsilon' as defined in the elliptical estimation

--- scripts/households.py
+++ scripts/households.py
@@ -8,13 +8,14 @@ population dynamics is rho_s, which has
 been added.
 '''
 
-import numpy as np
+import upsilonmpy as np
 import elliptical_u_est as ellip
 
 # household functions
 
 
-def FOCs(b_sp1, n_s, *args):
+def FOCs(b_sp1, *params):
+    # def FOCs(b_sp1, n_s, *args):
     '''
     For the S-period problem, we have 2S-1 FOCs corresponding to the savings
     variable b, and the labor supply variable n
@@ -31,6 +32,7 @@ def FOCs(b_sp1, n_s, *args):
             provide initial guesses for this variable.
     n_s: The labor supply values for each period. The call to this function
             should provide initial guesses for this variable.
+    BQ_path: path of bequests
     l_tilde: maximum amount of labor supply
     chi: scale parameter
     theta: Frisch elasticity of labor supply
@@ -45,29 +47,20 @@ def FOCs(b_sp1, n_s, *args):
     the next S values are n1, n2, ..., nS.
     '''
 
-    beta, sigma, r, w, b_init, l_tilde, chi, theta, rho_s = args
-    # When working on SS.py, note that b_sp1_guess is now of length S-1
+    BQpath, rho_s, beta, sigma, b_init, n, rpath, wpath = params
+    # rpath = length p, n = length p
+    # p = upsilonmber of periods left in life time
+    # wpath = length p, b_sp1 = length p-1
+
     b_s = np.append(b_init, b_sp1)
     b_sp1 = np.append(b_sp1, 0.0)
-    c = get_c(r, w, n_s, b_s, b_sp1)
-    mu_c = mu_cons(c, sigma)
-    mu_n = mu_labor(n_s, l_tilde, chi, theta)
 
-    # First get the Euler equations defined by Equation (4.10) - S-1 of these
-    lhs_euler_b = mu_c
-    rhs_euler_b = beta * (1+r) * mu_c
-    foc_errors_b = lhs_euler_b[:-1] - rhs_euler_b[1:]
-    # The above line doesn't need to be changed since it works for a general S
-    # as well (writing out the vectors helps to see this)
+    c = (1 + rpath) * b_s + wpath * n + BQpath - b_sp1
+    MU_c = mu_cons(c, sigma)
+    errors = (MU_c[:-1] - beta * (1 + rpath[1:]) *
+              (1 - rho_s[:-1]) * MU_c[1:])
 
-    # Next get the Euler equations defined by (4.9) - S of these
-    lhs_euler_n = w * mu_c
-    rhs_euler_n = mu_n
-    foc_errors_n = lhs_euler_n - rhs_euler_n
-
-    foc_errors = np.append(foc_errors_b, foc_errors_n)
-
-    return foc_errors
+    return errors
 
 
 def get_c(r, w, n_s, b_s, b_sp1):
@@ -96,10 +89,10 @@ def mu_labor(n_s, l_tilde, chi, theta):
     Computes marginal utility with respect to labor supply
     '''
 
-    b_ellipse, nu = ellip.estimation(theta, l_tilde)
+    b_ellipse, upsilon = ellip.estimation(theta, l_tilde)
     # b_ellipse is the constant defined in Equation (4.9) - has nothing to do
     # with savings
-    mu_n = chi * (b_ellipse / l_tilde) * (n_s / l_tilde) ** (nu - 1) * (
-                                 1 - (n_s / l_tilde) ** nu) ** ((1 - nu) / nu)
+    mu_n = chi * (b_ellipse / l_tilde) * (n_s / l_tilde) ** (upsilon - 1)
+    * (1 - (n_s / l_tilde) ** upsilon) ** ((1 - upsilon) / upsilon)
 
     return mu_n
